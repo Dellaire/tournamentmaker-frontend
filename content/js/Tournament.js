fad701e988c2d1a519841b031fe0f91497c70df3
Dellaire <Henry@DESKTOP-SSS9VH7>
2017-07-12 23:00:31 +0200

    added display of all matches of first round of first tournament

--- content/js/Tournament.js
+++ content/js/Tournament.js
@@ -1,9 +1,24 @@
-function fillTournamentTable() {
-	document.getElementById("tournaments").innerHTML = "";
+function fillMatchTable() {
+	document.getElementById("matches").innerHTML = "";
+	addTableHeader();
 
-	var table = document.getElementById("tournaments");
-	var row = table.insertRow(0);
-	var cell = row.insertCell(0);
-	cell.innerHTML = JSON
-			.stringify(getContent("http://localhost:8080/tournaments"));
+	var matches = getContent("http://localhost:8080/tournaments")[0].rounds[0].matches;
+	for (var i = 0; i < matches.length; i++) {
+		addMatchToTable(matches[i], i);
+	}
+}
+
+function addMatchToTable(match, row) {
+	var table = document.getElementById("matches");
+	var row = table.insertRow(row);
+
+	var team1Cell = row.insertCell(0);
+	var scoreCell = row.insertCell(1);
+	var team2Cell = row.insertCell(2);
+
+	team1Cell.innerHTML = match.team1.player1.name + " + "
+			+ match.team1.player2.name;
+	scoreCell.innerHTML = match.team1Score + " : " + match.team2Score;
+	team2Cell.innerHTML = match.team2.player1.name + " + "
+			+ match.team2.player2.name;
 }
