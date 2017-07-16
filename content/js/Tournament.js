de4db79522ecd5eb9dacea7c4afeb85732b57ba0
Dellaire <Henry@DESKTOP-SSS9VH7>
2017-07-16 12:04:35 +0200

    added display of all matches

--- content/js/Tournament.js
+++ content/js/Tournament.js
@@ -2,20 +2,49 @@ function fillMatchTable() {
 	document.getElementById("matches").innerHTML = "";
 	addTableHeader();
 
-	var matches = getContent("http://localhost:8080/tournaments")[0].rounds[0].matches;
-	for (var i = 0; i < matches.length; i++) {
-		addMatchToTable(matches[i], i);
+	addMatchesTableHeader();
+
+	var tournaments = getContent("http://localhost:8080/tournaments");// [0].rounds[0].matches;
+	for (var i = 0; i < tournaments.length; i++) {
+		for (var j = 0; j < tournaments[i].rounds.length; j++) {
+			for (var k = 0; k < tournaments[i].rounds[j].matches.length; k++) {
+				addMatchToTable(tournaments[i].name,
+						tournaments[i].rounds[j].name,
+						tournaments[i].rounds[j].matches[k], k);
+			}
+		}
 	}
 }
 
-function addMatchToTable(match, row) {
+function addMatchesTableHeader() {
 	var table = document.getElementById("matches");
-	var row = table.insertRow(row);
+	var row = table.insertRow(0);
 
-	var team1Cell = row.insertCell(0);
-	var scoreCell = row.insertCell(1);
-	var team2Cell = row.insertCell(2);
+	var tournamentCell = row.insertCell(0);
+	var roundCell = row.insertCell(1);
+	var team1Cell = row.insertCell(2);
+	var scoreCell = row.insertCell(3);
+	var team2Cell = row.insertCell(4);
 
+	tournamentCell.innerHTML = "Tournament name";
+	roundCell.innerHTML = "Round name";
+	team1Cell.innerHTML = "Team 1";
+	scoreCell.innerHTML = "vs.";
+	team2Cell.innerHTML = "Team 2";
+}
+
+function addMatchToTable(tournamentName, roundName, match, row) {
+	var table = document.getElementById("matches");
+	var row = table.insertRow(row + 1);
+
+	var tournamentCell = row.insertCell(0);
+	var roundCell = row.insertCell(1);
+	var team1Cell = row.insertCell(2);
+	var scoreCell = row.insertCell(3);
+	var team2Cell = row.insertCell(4);
+
+	tournamentCell.innerHTML = tournamentName;
+	roundCell.innerHTML = roundName;
 	team1Cell.innerHTML = match.team1.player1.name + " + "
 			+ match.team1.player2.name;
 	scoreCell.innerHTML = match.team1Score + " : " + match.team2Score;
