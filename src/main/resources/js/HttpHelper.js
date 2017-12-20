719811e9a708a05b332ce26bd9ea2d0da765aaf8
Dellaire <henry.borasch@freenet.de>
2017-12-21 00:17:17 +0100

    added CSRF token to POST and PUT requests

--- src/main/resources/js/HttpHelper.js
+++ src/main/resources/js/HttpHelper.js
@@ -3,20 +3,31 @@ function getContent(url) {
 	xmlhttp.open("GET", url, false);
 	xmlhttp.send();
 
+	var token = xmlhttp.getResponseHeader("X-CSRF-TOKEN")
+	document.querySelector('meta[name="_csrf"]').setAttribute("content", token);
+
 	return JSON.parse(xmlhttp.responseText);
 }
 
 function postContent(url, body) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("POST", url, false);
+
+	var token = $("meta[name='_csrf']").attr("content");
+	xmlhttp.setRequestHeader("X-CSRF-TOKEN", token);
 	xmlhttp.setRequestHeader("Content-Type", "application/json");
+
 	xmlhttp.send(JSON.stringify(body));
 }
 
 function putContent(url, body) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("PUT", url, false);
+
+	var token = $("meta[name='_csrf']").attr("content");
+	xmlhttp.setRequestHeader("X-CSRF-TOKEN", token);
 	xmlhttp.setRequestHeader("Content-Type", "application/json");
+
 	xmlhttp.send(JSON.stringify(body));
 }
 
