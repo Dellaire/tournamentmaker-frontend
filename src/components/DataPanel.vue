69c25c5a9e2d2253152b175f815c6ddfe845d987
Dellaire <henry.borasch@freenet.de>
2020-04-07 17:15:16 +0200

    added new layout layer

---
+++ src/components/DataPanel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <table>
+        <tr>
+            <td v-bind:key="element.label" v-for="element in firstRow">
+                <SectionPanel :content="element.label"/>
+            </td>
+        </tr>
+        <tr>
+            <td v-bind:key="element.label" v-for="element in secondRow">
+                <SectionPanel :content="element.label"/>
+            </td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import SectionPanel from './SectionPanel.vue'
+
+export default {
+  name: 'DataPanel',
+  components: {
+    SectionPanel
+  },
+  props: ["elements"],
+  computed: {
+    firstRow: function() {
+        return this.elements.slice(0, 2)
+    },
+    secondRow: function() {
+        return this.elements.slice(2, 4)
+    }
+  }
+}
+</script>
+
+<style scoped>
+table {
+    width: 100%;
+}
+</style>
